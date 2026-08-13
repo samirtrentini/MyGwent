46ef9cd4c983767f3e0b0d458639b31e8a7d6af5
Samir Trentini Zedan <samir.zedan@cpb.com.br>
2026-08-13 17:13:03 -0300

    fix: limpando banco ao iniciar app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:my_gwent/database/app_database.dart';
 import 'app/app.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await AppDatabase.instance.resetDatabase();
-
+void main() {
   runApp(const MyApp());
 }
